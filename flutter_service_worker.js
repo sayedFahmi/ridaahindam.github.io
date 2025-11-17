291d438f24e942ae25f3716a981cab325f0354a5
sayedFahmi <110277397+sayedFahmi@users.noreply.github.com>
2025-11-17 23:29:39 +0300

    Add files via upload
    Signed-off-by: sayedFahmi <110277397+sayedFahmi@users.noreply.github.com>

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -211,7 +211,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "401417400debfeda021e54cb3f8b8f9a
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "8920ede859dcc2bfa35b42513b9af24e",
+"flutter_bootstrap.js": "9f9304f849732620bc627f9c69400fa7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
