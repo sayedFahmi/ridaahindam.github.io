3e9d78f81ac173b238432d45606f74c67ee4d5b7
sayedFahmi <110277397+sayedFahmi@users.noreply.github.com>
2025-12-04 10:24:59 +0300

    Add files via upload
    Signed-off-by: sayedFahmi <110277397+sayedFahmi@users.noreply.github.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3617948828"
+    serviceWorkerVersion: "2744187511"
   }
 });
